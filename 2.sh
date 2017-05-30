187e18d1be1957413dd37de698f683a1a0b40c86
Takaaki-Inaba <g.takaaki.inaba@gmail.com>
2017-05-30 15:29:21 +0900

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -4,7 +4,7 @@ pip install django
 echo "<<<<<<<<<<<<<<<<<<<< python & django install success!! >>>>>>>>>>>>>>>>>>" > /root/setting.log
 
 #django projectの作成
-cd /var/www/cgi-bin/; django-admin.py startproject test_proj
+(cd /var/www/cgi-bin/; django-admin.py startproject test_proj)
 
 #Apacheのインストール
 yum -y install httpd httpd-devel
@@ -15,7 +15,9 @@ wget https://github.com/GrahamDumpleton/mod_wsgi/archive/4.5.14.tar.gz
 tar -zxvf 4.5.14.tar.gz
 
 RHOME="/root"
-$RHOME/mod_wsgi-4.5.14/configure CFLAGS=-fPIC --enable-shared --with-python=/root/.pyenv/versions/3.6.1/bin/python　&& make && make install
+$RHOME/mod_wsgi-4.5.14/configure CFLAGS=-fPIC --enable-shared --with-python=/root/.pyenv/versions/3.6.1/bin/python
+(cd $RHOME/mod_wsgi-4.5.14; make) 
+(cd $RHOME/mod_wsgi-4.5.14; make install)
 ln -sf /root/.pyenv/versions/3.6.1/lib/libpython3.6m.so.1.0 /lib64/
 
 
