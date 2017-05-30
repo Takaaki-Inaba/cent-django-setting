f8d03bc2a09f226a4226b845b4fd5320adabcd8c
Takaaki-Inaba <g.takaaki.inaba@gmail.com>
2017-05-30 11:42:42 +0900

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -16,8 +16,8 @@ tar -zxvf 4.5.14.tar.gz
 
 RHOME="/root"
 $RHOME/mod_wsgi-4.5.14/configure CFLAGS=-fPIC --enable-shared --with-python=/root/.pyenv/versions/3.6.1/bin/python
-mod_wsgi-4.5.14/make
-mod_wsgi-4.5.14/make install
+$RHOME/mod_wsgi-4.5.14/make
+$RHOME/mod_wsgi-4.5.14/make install
 ln -sf /usr/local/pyenv/versions/3.6.1/lib/libpython3.6m.so.1.0 /lib64/
 
 
